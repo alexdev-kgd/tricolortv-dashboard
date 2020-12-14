919182cfbcaf73f218af3358db03bd0041bfe97f
Alexdev2019 <alexpetrov248@gmail.com>
2020-12-14 20:19:18 +0200

    First commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [
+    DefaultComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ]
+})
+export class DefaultModule { }
