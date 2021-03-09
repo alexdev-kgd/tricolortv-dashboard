c78bdfc548e11d692ea1fabafd8354fdadd4a625
Alexdev2019 <alexpetrov248@gmail.com>
2021-03-09 02:27:43 +0200

    Added staff section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModulesComponent } from './pages/modules/modules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ModulesComponent,
   ],
   imports: [
     BrowserModule,
