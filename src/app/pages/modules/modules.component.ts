e8c70b4f6dd2b3f5cadd889c2293bbf595ada161
Alexdev2019 <alexpetrov248@gmail.com>
2021-04-09 20:25:34 +0200

    Added paginator to modules page

--- src/app/pages/modules/modules.component.ts
+++ src/app/pages/modules/modules.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { PaginatorModulesService } from './paginator-modules.service';
 
 @Component({
   selector: 'app-modules',
   templateUrl: './modules.component.html',
   styleUrls: ['./modules.component.sass'],
+  providers: [{
+    provide: MatPaginatorIntl,
+    useClass: PaginatorModulesService
+  }]
 })
 export class ModulesComponent {
+  // MatPaginator Inputs
+  length = 50;
 
+  pageSize = 10;
 }
