057ef4d807ac8f2fd0cf853d627f0277322a8dbb
Alexdev2019 <alexpetrov248@gmail.com>
2021-12-30 23:24:11 +0200

    Font Size Input Implementation

--- src/app/shared/components/input-number/input-number.component.ts
+++ src/app/shared/components/input-number/input-number.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+enum CALCULATING_OPERATIONS {
+  DECREMENT = "DECREMENT",
+  INCREMENT = "INCREMENT"
+}
+const DEFAULT_FONT_SIZE = 14;
 
 @Component({
   selector: 'input-number',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-number.component.sass']
 })
 export class InputNumberComponent implements OnInit {
+  @ViewChild('decrement') decrement: ElementRef;
+  @ViewChild('increment') increment: ElementRef;
+  @ViewChild('numberInput', { static: true }) numberInput: ElementRef;
 
-  constructor() { }
+  @Input('min') min: number;
+  @Input('max') max: number;
 
   ngOnInit(): void {
+      this.numberInput.nativeElement.value = DEFAULT_FONT_SIZE;
+  }
+
+  public calculateNumber(operation: CALCULATING_OPERATIONS, predicate): void {
+    if(!predicate) return;
+
+    let value = this.numberInput.nativeElement.value;
+
+    switch (operation) {
+      case "INCREMENT":
+        value++;
+        break;
+      case "DECREMENT":
+        value--;
+        break;
+      default:
+        break;
+    }
+    
+    this.numberInput.nativeElement.value = value;
+  }
+
+  public decrementNumber() {
+    const predicate = !this.min || this.numberInput.nativeElement.value > this.min;
+    this.calculateNumber(CALCULATING_OPERATIONS.DECREMENT, predicate);
+  }
+
+  public incrementNumber() {
+    const predicate = !this.max || this.numberInput.nativeElement.value < this.max;
+    this.calculateNumber(CALCULATING_OPERATIONS.INCREMENT, predicate);
   }
 
 }
