6ff644522c02e7291397daefff9a88bcb521f57e
Aleksandr Petrov <aleksandr.petrov@info-pro.ru>
2023-11-06 16:18:29 +0200

    Angular Migration to Version 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { ThemeService } from '@services/theme.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DefaultModule } from './layouts/default/default.module';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    DefaultModule,
+    CommonModule
+  ],
+  standalone: true,
 })
 export class AppComponent implements OnInit {
   title = 'tricolortv-dashboard';
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,99 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ArticlesComponent } from '@pages/articles/articles.component';
+import { CommentsComponent } from '@pages/comments/comments.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { ModulesComponent } from '@pages/modules/modules.component';
+import { SettingsComponent } from '@pages/settings/settings.component';
+import { StaffComponent } from '@pages/staff/staff.component';
+import { StatisticsComponent } from '@pages/statistics/statistics.component';
+import { AppComponent } from './app/app.component';
+import { DefaultComponent } from './app/layouts/default/default.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+export default function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+        data: { animation: 'HomePage' },
+      },
+      {
+        path: 'stats',
+        component: StatisticsComponent,
+        data: { animation: 'StatsPage' },
+      },
+      {
+        path: 'articles',
+        component: ArticlesComponent,
+        data: { animation: 'ArticlesPage' },
+      },
+      {
+        path: 'staff',
+        component: StaffComponent,
+        data: { animation: 'StaffPage' },
+      },
+      {
+        path: 'modules',
+        component: ModulesComponent,
+        data: { animation: 'ModulesPage' },
+      },
+      {
+        path: 'comments',
+        component: CommentsComponent,
+        data: { animation: 'CommentsPage' },
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent,
+        data: { animation: 'SettingsPage' },
+      },
+    ],
+  },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([HttpClientModule, BrowserAnimationsModule]),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'ru',
+      })
+    ),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic',
+      },
+    },
+  ],
+});
